b2eba7e048c261dd411379b43c3473744a34919b
Garance CLAUSSE <gclausse@e1r7p17.clusters.42paris.fr>
2022-09-12 18:59:50 +0200

    Reinterpret cast

--- ex01/Data.cpp
+++ ex01/Data.cpp
@@ -6,7 +6,7 @@
 /*   By: gclausse <gclausse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/12 16:25:03 by gclausse          #+#    #+#             */
-/*   Updated: 2022/09/12 18:58:15 by gclausse         ###   ########.fr       */
+/*   Updated: 2022/09/12 18:59:38 by gclausse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@
 
 uintptr_t serialize(Data* ptr)
 {
-	
+	uintptr_t serie = reinterpret_cast<uintptr_t>(ptr);
 }
