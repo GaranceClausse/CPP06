1aeb64157870d624941995ab74caee4bdfb8966d
Garance CLAUSSE <gclausse@e1r3p16.clusters.42paris.fr>
2022-09-14 12:04:03 +0200

    Random base

--- ex02/Base.cpp
+++ ex02/Base.cpp
@@ -6,11 +6,14 @@
 /*   By: gclausse <gclausse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 11:34:51 by gclausse          #+#    #+#             */
-/*   Updated: 2022/09/14 11:40:23 by gclausse         ###   ########.fr       */
+/*   Updated: 2022/09/14 12:03:48 by gclausse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
 
 
 Base::~Base()
@@ -21,7 +24,19 @@ Base::~Base()
 
 Base * generate(void)
 {
-	
+	Base *randomBase;
+	srand( time(NULL) );
+	int randNum = rand() % 2;
+	switch (randNum)
+	{
+		case 0 : 
+		randomBase = new A;
+		case 1 :
+		randomBase = new B;
+		case 2 : 
+		randomBase = new C;
+	}
+	return (randomBase);
 }
 
 void identify(Base* p)
